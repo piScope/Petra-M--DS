100774d990288d00766555e7d671fa309359f8e7
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2025-10-21 10:06:22 -0400

    initial commit

---
+++ _build_system/build_global.py
@@ -0,0 +1,9 @@
+#
+#  build global parameters
+#
+__all__ = ("mumps_build_opts",)
+
+
+mumps_build_opts = {"mumps_cmake":"https://github.com/scivision/mumps",
+                    "mumps_version":"5.8.1",
+                    "int64": False}
